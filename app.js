35e0920513e3be29130dc0f71be7fe50b1180654
AJ Dorfman <ajdorfman91@gmail.com>
2023-03-21 10:06:17 -0700

    finish exercise

--- app.js
+++ app.js
@@ -26,8 +26,66 @@ class Person{
     greeting(){
         console.log(`Hello ${this.name}, welcome to existance!`)}
 }
+// ex 2
+class Coder extends Person {
+    constructor(name,pets,residence, hobbies, occupation){
+        super(name,pets,residence,hobbies);
+        
+        this.occupation = occupation;
+        occupation= "Full Stack Web Developer";
 
-const ross = new Person("Ross",2, "birmingham",["kayak fishing, solving code","looking cool"])
+    }
+    greeting(){
+        console.log(`Hello ${this.name}, congrats on becoming a coder!`);
+    }
+    }
 
 
 
+
+
+//  ex 3
+
+const ross = new Person("Ross",2, "birmingham",["kayak fishing", "solving code","looking cool"]);
+const aj = new Coder("AJ", 1, "Marina Del Rey", ["golf", "hockey"],"full stack web developer");
+
+console.log(ross);
+ross.greeting();
+ross.addHobby("lure making");
+console.log(aj);
+aj.greeting();
+
+
+// ex 4
+
+class Calculator{
+    cosntructor(){
+        this.result= 0;
+    }
+    add(a,b = this.result){
+        this.result = a +b;
+        return this.result;
+    }
+    subtract(a,b = this.result){
+        this.result = a - b;
+        return this.result;
+    }
+    multiply(a,b = this.result){
+        this.result = a * b;
+        return this.result;
+    }
+    divide(a,b = this.result){
+        this.result = a / b;
+        return this.result;
+    }
+    displayResults(){
+        console.log(this.result);
+    }
+}
+
+const calculator = new Calculator();
+
+calculator.add (7,3);
+
+
+console.log(calculator);
